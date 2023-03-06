2bb8c7b3a3beb4598a63c70a43a99cc48b644b46
Prabhatlodhi <lodhi.prabhat@gmail.com>
2023-03-07 00:07:43 +0530

    aa

---
+++ App.js
@@ -0,0 +1,12 @@
+
+
+const heading3 = React.createElement("h3",{className:"koko"}, "333333 content I want to put inside the taggggggggggggg");
+// const heading2 = React.createElement("h2",{}, "2222 content I want to put inside the tah");
+// const heading = React.createElement("div",{key:2}, [heading2, heading3]);
+// const ccc = React.createElement("div",{key:3}, [heading]);
+
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(heading3)
